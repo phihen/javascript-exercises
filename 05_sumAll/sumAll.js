d68e062716264b38ef099e291dad66a5acbd961d
phihen <48790552+phihen@users.noreply.github.com>
2025-04-25 13:54:47 +0200

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(numberA, numberB) {
+    if(numberA < 0 || numberB < 0 || !Number.isInteger(numberA) || !Number.isInteger(numberB)) {
+        return "ERROR";
+    }
 
+    let startNumber = Math.min(numberA, numberB);
+    let endNumber = Math.max(numberA, numberB);
+    let sum = 0;
+    for(let i = startNumber; i <= endNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
