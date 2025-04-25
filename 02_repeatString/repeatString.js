480b9f08172423f93201de86200bd3ad197f0484
phihen <48790552+phihen@users.noreply.github.com>
2025-04-25 12:03:19 +0200

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, numberOfRepeats) {
+    if(numberOfRepeats < 0) {
+        return "ERROR";
+    }
 
+    let returnText = "";
+    for(let i = 0; i<numberOfRepeats; i++) {
+        returnText += text;
+    }
+
+    return returnText;
 };
 
 // Do not edit below this line
