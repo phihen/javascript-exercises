98f159d5fdefef2b2e28e40c829192c64afb4515
phihen <48790552+phihen@users.noreply.github.com>
2025-08-17 17:03:30 +0200

    solve exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    return people.reduce(getOldest);
 };
+
+function getOldest(oldest, nextPerson) {
+    let oldestAge = getAge(oldest);
+    let nextPersonAge = getAge(nextPerson);
+
+    return oldestAge > nextPersonAge ? oldest : nextPerson;
+}
+
+function getAge(person) {
+    return person.yearOfDeath != undefined ? (person.yearOfDeath - person.yearOfBirth): (new Date().getFullYear() - person.yearOfBirth);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
