62923b839787fe6e0ab955003af5f965bb17fd1c
phihen <48790552+phihen@users.noreply.github.com>
2025-08-17 12:12:02 +0200

    solve exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(memberIndex) {
 
+    let count;
+
+    if (typeof memberIndex !== 'number') {
+        count = parseInt(memberIndex)
+    } else {
+        count = memberIndex
+    }
+
+    if(count < 0) {
+        return "OOPS";
+    }
+
+    if(count === 0) {
+        return 0;
+    }
+
+    if(count === 1) {
+        return 1;
+    }
+
+    return fibonacci(count-2) + fibonacci(count-1);
 };
 
 // Do not edit below this line
