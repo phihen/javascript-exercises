e78ae56896baa65b94f1cc3ee9ad37a47cfb82a4
phihen <48790552+phihen@users.noreply.github.com>
2025-08-17 11:34:17 +0200

    solve exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let formattedText = formatText(text);
+    let formattedTextReversed = formattedText.split('').reverse().join('');
 
+    return formattedText === formattedTextReversed;
 };
+
+function formatText(text) {
+  let punctuation = /[\.,?!]/g;
+  let newText = text.replace(punctuation, "");
+  newText = newText.toLowerCase();
+  newText = newText.replace(/\s/g, '');
+  return newText;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
